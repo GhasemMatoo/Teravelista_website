01a881c0b9bbe7fd6728339a8bbc8590859dfca3
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2021-10-31 16:48:28 +0330

    change mode filter post
    to views QuerySet  Filter mod to change exclude mod

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def blog_views(requset):
     time_now=datetime.datetime.now()
     formatedDate = time_now.strftime("%Y-%m-%d")
     #Posts = post.objects.filter(published_date__gt=datetime.datetime.now(timezone.utc), status=1)#by timezone
-    Posts = post.objects.filter(published_date__gt=formatedDate, status=1)
+    Posts = post.objects.exclude(published_date__gt=formatedDate, status=1)
     context = {'Posts': Posts}
     return render(requset, 'blog/blog-home.html', context)
 
@@ -17,7 +17,8 @@ def blog_views(requset):
 def single_views(requset,pid):
     time_now=datetime.datetime.now()
     formatedDate = time_now.strftime("%Y-%m-%d")
-    Post =get_object_or_404(post,published_date__gt=formatedDate, status=1,id=pid)
+    Posts = post.objects.exclude(published_date__gt=formatedDate, status=1)
+    Post =get_object_or_404(Posts,id=pid)
     Post.counted_views= Post.counted_views +1
     Post.save() 
     context = {'Post': Post}
