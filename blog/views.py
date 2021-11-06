243080bb354b41830a44c83c74a877e933a1b413
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2021-11-06 19:03:50 +0330

    paginator
    create blog-pagination for method pagination

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,45 @@
-from django.shortcuts import render,get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import render, get_object_or_404
 from blog.models import post
 from datetime import datetime, timedelta
 # Create your views here.
 
 
-def blog_views(request,cat_name=None,author_username=None):
-    time_now=datetime.now() + timedelta(days=1)
+def blog_views(request, cat_name=None, author_username=None):
+    time_now = datetime.now() + timedelta(days=1)
     formatedDate = time_now.strftime("%Y-%m-%d")
-    Posts = post.objects.exclude(published_date__gt=formatedDate).filter(status=1)
+    Posts = post.objects.exclude(
+        published_date__gt=formatedDate).filter(status=1)
     if cat_name:
-        Posts=Posts.filter(category__name=cat_name)
+        Posts = Posts.filter(category__name=cat_name)
     if author_username:
-        Posts=Posts.filter(author__username=author_username)
+        Posts = Posts.filter(author__username=author_username)
+    Posts = Paginator(Posts, 3)
+    page_number = request.GET.get('page')
+    Posts=Posts.get_page(page_number)
     context = {'Posts': Posts}
     return render(request, 'blog/blog-home.html', context)
 
 
-def single_views(request,pid):
-    time_now=datetime.now() + timedelta(days=1)
+def single_views(request, pid):
+    time_now = datetime.now() + timedelta(days=1)
     formatedDate = time_now.strftime("%Y-%m-%d")
-    Posts = post.objects.exclude(published_date__gt=formatedDate).filter(status=1)
-    Post =get_object_or_404(Posts,id=pid)
-    Post.counted_views= Post.counted_views +1
-    Post.save() 
-    context = {'Post': Post,'Posts': Posts}
-    return render(request, 'blog/blog-single.html',context)
+    Posts = post.objects.exclude(
+        published_date__gt=formatedDate).filter(status=1)
+    Post = get_object_or_404(Posts, id=pid)
+    Post.counted_views = Post.counted_views + 1
+    Post.save()
+    context = {'Post': Post, 'Posts': Posts}
+    return render(request, 'blog/blog-single.html', context)
+
 
 def search_views(request):
-    time_now=datetime.now() + timedelta(days=1)
+    time_now = datetime.now() + timedelta(days=1)
     formatedDate = time_now.strftime("%Y-%m-%d")
-    Posts = post.objects.exclude(published_date__gt=formatedDate).filter(status=1)
+    Posts = post.objects.exclude(
+        published_date__gt=formatedDate).filter(status=1)
     if request.method == 'GET':
-         Posts=Posts.filter(content__contains=request.GET.get("s"))
+        if s := request.GET.get("s"):
+            Posts = Posts.filter(content__contains=s)
     context = {'Posts': Posts}
     return render(request, 'blog/blog-home.html', context)
