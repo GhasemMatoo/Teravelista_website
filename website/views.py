d1db5ff392e63a6ed571ac6838f3c8201545446f
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2021-11-14 21:00:11 +0330

    form message
    create form  message  alert

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from website.form import ContactForm,NewsletterForm
+from django.contrib import messages
 # Create your views here.
 def index_views(request):
     return render(request,'website/index.html')
@@ -14,6 +15,9 @@ def contact_views(request):
         if form.is_valid():
             form.instance.name = form.cleaned_data['name']='UNKNOW'
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'Yor ticket submited successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'Yor ticket dident submited.')
     form= ContactForm()
     return render(request,'website/contact.html',{'form': form})
 
